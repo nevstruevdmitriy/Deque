71dd65e34fdd73da271cb2b20e6c6d6b7c43ad03
nevstruevdmitriy <nevstruev.dmitriy@mail.ru>
2016-11-22 20:29:36 +0300

    Update main.cpp

--- deque2/main.cpp
+++ deque2/main.cpp
@@ -2,7 +2,31 @@
 #include "gtest\gtest.h"
 #include <algorithm>
 
-TEST(testReverseIterator, CheckIt){
+void testTime(const int MAXN){
+    Deque <int> d;
+    int ind = 0;
+    for (int i = 0; i < MAXN; ++i) {
+        d.push_back(i);
+    }
+    while (!d.empty()) {
+        d.pop_front();
+    }
+}
+
+TEST(testTime0, CheckIt) {
+    testTime(100000);
+}
+TEST(testTime1, CheckIt) {
+    testTime(1000000);
+}
+TEST(testTime2, CheckIt) {
+    testTime(10000000);
+}
+TEST(testTime3, CheckIt) {
+    testTime(100000000);
+}
+
+TEST(testReverseIterator, CheckIt) {
     const int MAXN = 10000000;
 
     Deque <int> d;
@@ -13,19 +37,6 @@ TEST(testReverseIterator, CheckIt){
     for (Deque <int>::reverse_iterator i = d.rend() - 1; i - d.rbegin() > 0; --i) {
         ASSERT_EQ(*i, ind);
         ++ind;
-    }
-}
-
-TEST(testTime, CheckDeque) {
-    const int MAXN = 10000000;
-
-    Deque <int> d;
-    for (int i = 0; i < MAXN; ++i) {
-        d.push_back(i);
-    }
-    for (int i = 0; i < 1000; i++) {
-        int ind = rand() % MAXN;
-        ASSERT_EQ(d[ind], ind);
     }
 }
 TEST(testIterator, CheckDeque) {
@@ -52,21 +63,24 @@ TEST(testIterator, CheckDeque) {
         ASSERT_EQ(*i, ind);
     }
 }
-TEST(testPushPop, CheckDeque){
+TEST(testPushPop, CheckDeque) {
     const int MAXN = 10000000;
     Deque <int> d;
     for (int i = 0; i < MAXN; ++i) {
         if (rand() & 1) {
             if (rand() & 1) {
                 d.push_back(1);
-            }else {
+            }
+            else {
                 d.push_front(1);
             }
-        }else {
+        }
+        else {
             if (!d.empty()) {
                 if (rand() & 1) {
                     d.pop_back();
-                }else{
+                }
+                else {
                     d.pop_front();
                 }
             }
@@ -79,15 +93,6 @@ TEST(testPushPop, CheckDeque){
     for (int i = 0; i < d.size(); i++) {
         ASSERT_EQ(d[i], 1);
     }
-}
-TEST(time, CheckSort){
-    const int MAXN = 10000000;
-    int *a = new int[MAXN];
-    for(int i = 0; i < MAXN; ++i) {
-        a[i] = i;
-    }
-    std::sort(a, a + MAXN);
-    delete[] a;
 }
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
